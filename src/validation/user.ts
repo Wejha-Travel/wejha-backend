4dae7a0cf153f55d363f003a93393403952845dc
Mohamed Belkheir <mohamedbel@protonmail.com>
2021-04-02 18:45:30 +0200

    user validation fixes

--- src/validation/user.ts
+++ src/validation/user.ts
@@ -1,12 +1,10 @@
 import Joi from "joi";
 import { ValidationError } from "../exceptions";
 import { User } from "../models/interfaces/user";
-import { userInfo } from "node:os";
 
 export const UserSchema = Joi.object({
-  username: Joi.string().alphanum().min(3).max(30).required(),
-
-  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
+  username: Joi.string().alphanum().min(3).max(30),
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
   email: Joi.string().email({
     minDomainSegments: 2,
     tlds: { allow: ["com", "net"] },
@@ -14,10 +12,9 @@ export const UserSchema = Joi.object({
 });
 
 export function CreateUserValidation(data: any) {
-    let { value, error } = UserSchema.validate(data, {presence: "required"});
+    let { value, error } = UserSchema.validate(data, { presence: "required" });
     if (error) throw new ValidationError(error.message);
-    console.log(value);
-    return value as Omit<User, "id" |"password">;
+    return value as Omit<User, "id">;
 }
 export function editUserValidation(data: any) {
     let { value, error } = UserSchema.validate(data);
