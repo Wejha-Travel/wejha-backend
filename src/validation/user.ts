365ac37fa07fee848ae4188afa372d152df63ab8
Hamed EL-Asma <hamedelasma@gmail.com>
2021-04-02 18:23:04 +0200

    removie status validation

--- src/validation/user.ts
+++ src/validation/user.ts
@@ -7,7 +7,6 @@ export const UserSchema = Joi.object({
   username: Joi.string().alphanum().min(3).max(30).required(),
 
   password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
-  status: Joi.string().valid("active", "unverified", "banned"),
   email: Joi.string().email({
     minDomainSegments: 2,
     tlds: { allow: ["com", "net"] },
