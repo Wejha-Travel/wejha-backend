2aa6730be1409741d92275b9ff72a174a3925d61
Mohamed Belkheir <mohamedbel@protonmail.com>
2021-04-04 11:47:13 +0200

    added missing cors request

--- src/application/express/index.ts
+++ src/application/express/index.ts
@@ -2,12 +2,13 @@ import Express from 'express';
 import routers from './api';
 import errorHandler from './error';
 import { config } from '../../dependency/config';
+import cors from "cors";
 
 const server = Express();
 const port = config.server.port;
     
 server.use(Express.json())
-
+server.use(cors());
 server.use('/api', routers);
 
 server.use(errorHandler)
